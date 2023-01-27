d6793616ed11227a03b5950119cc3825bd10d008
rza-git <rezabrains82@gmail.com>
2023-01-27 20:19:50 +0700

    feature register finished

--- featureRegister.js
+++ featureRegister.js
@@ -1,1 +1,11 @@
 // B
+
+const register = (userName, password, birthYear) => {
+
+    console.log(`Created user with`)
+    console.log(`username: ${userName}`)
+    console.log(`password: ${password}`)
+    console.log(`birthYear: ${birthYear}`)
+}
+
+register("new_user", "password01", 1865)
